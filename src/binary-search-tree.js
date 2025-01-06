e85a2f403aad5ba839100c46d1dbbb2b4fe0ffa6
madsok <madsok25@gmail.com>
2025-01-06 23:50:26 +0300

    feat: add methods

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,31 +1,76 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.treeRoot = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.treeRoot;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.treeRoot = addNode(this.treeRoot, data);
+
+    function addNode(node, value) {
+      if (node === null) {
+        return new Node(value);
+      }
+
+      if (value < node.data) {
+        node.left = addNode(node.left, value);
+      } else {
+        node.right = addNode(node.right, value);
+      }
+    
+      return node;
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return hasNode(this.treeRoot, data);
+
+    function hasNode(node, value) {
+      if (node === null) {
+        return false;
+      }
+
+      if (node.data === value) {
+        return true;
+      }
+
+      if (value < node.data) {
+        return hasNode(node.left, value);
+      } else {
+        return hasNode(node.right, value);
+      }
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    return findNode(this.treeRoot, data);
+
+    function findNode(node, value) {
+      if (node === null) {
+        return null;
+      }
+
+      if (value === node.data) {
+        return node;
+      }
+
+      if (value < node.data) {
+        return findNode(node.left, value);
+      } else {
+        return findNode(node.right, value);
+      }
+    }
   }
 
   remove(/* data */) {
